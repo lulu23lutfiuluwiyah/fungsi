c2339c3a95e7d2f05de7cbd8d9da95fcc902b25d
lulu23lutfiuluwiyah <lutfiuluwiyah88@gmail.com>
2024-11-26 10:00:44 +0700

    Add files via upload

---
+++ tugas punction.php
@@ -0,0 +1,15 @@
+<?php
+//mengembalikqan nilai 
+function tampilan_nama()
+{
+    echo "Hallo php";
+}
+tampilan_nama();
+
+//tidak mengembalikan nilai
+function penjumlahan($angka1, $angka2) {
+    $jumlah = $angka1 + $angka2;
+    echo  $jumlah;
+}
+penjumlahan(10, 20);
+?>
